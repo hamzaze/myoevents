024f334ad208ed7c06ec5e470d43a0e4e258ca44
Hamza Hrnjicevic <hamzazee@gmail.com>
2017-11-23 00:27:34 +0100

    FCM Sender ID registration

--- js/my-app.js
+++ js/my-app.js
@@ -66,6 +66,20 @@ var photoNavbarTemplate='<div class="navbar"> \
 </div>';
 
 
+var push = PushNotification.init({ "android": {"senderID": "931972740218"}} );
+push.on('registration', function(data) {
+alert(data.registrationId);
+});
+
+push.on('notification', function(data) {
+alert(data.title+" Message: " +data.message);
+});
+
+push.on('error', function(e) {
+alert(e);
+});
+
+
 if(!checkCookie()){
     mainView.router.load({
         template: Template7.templates.loginTemplate,
